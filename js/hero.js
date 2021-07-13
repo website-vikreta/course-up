351d413793958788354668a651440d5ff76e1fe1
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2021-07-13 13:34:07 +0530

    optimization in progress

--- js/hero.js
+++ js/hero.js
@@ -1,40 +1,84 @@
 // ? isotope filtering
 // ? ====================================================
-
-// init Isotope
-var $coursesSlider = $('.coursesSlider').isotope({
-   itemSelector: '.card',
-   layoutMode: 'fitRows'
-});
-// filter functions
-var filterFns = {
-   // show if number is greater than 50
-   numberGreaterThan50: function () {
-      var number = $(this).find('.number').text();
-      return parseInt(number, 10) > 50;
-   },
-   // show if name ends with -ium
-   ium: function () {
-      var name = $(this).find('.name').text();
-      return name.match(/ium$/);
-   }
-};
-// bind filter button click
-$('.filters-button-group').on('click', 'button', function () {
-   var filterValue = $(this).attr('data-filter');
-   // use filterFn if matches value
-   filterValue = filterFns[filterValue] || filterValue;
-   $coursesSlider.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$('.button-group').each(function (i, buttonGroup) {
-   var $buttonGroup = $(buttonGroup);
-   $buttonGroup.on('click', 'button', function () {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $(this).addClass('is-checked');
-   });
+$('.coursesSlider').slick({
+   dots: false,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 4,
+   slidesToScroll: 2,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   responsive: [
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 3,
+         }
+      },
+      {
+         breakpoint: 600,
+         settings: {
+            slidesToShow: 2,
+         }
+      },
+      {
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+         }
+      }
+   ]
 });
 
+$(".filterBtn").on("click", function () {
+   $(".filterBtn").removeClass("is-checked");
+   $(this).addClass("is-checked");
+
+   // get filter value
+   const data = $(this).attr("data-filter");
+
+   // filter
+   $('.coursesSlider').slick('slickUnfilter');
+   $('.coursesSlider').slick('slickFilter', data);
+})
+
+function filterCourse(className) {
+
+}
+
+
+// ? Hero attached slider
+// ? ====================================================
+// $('.hero-slider').slick({
+//    slidesToShow: 1,
+//    slidesToScroll: 1,
+//    arrows: false,
+//    fade: true,
+//    // asNavFor: '.hero-slider-ill'
+// });
+// $('.hero-slider-ill').slick({
+//    slidesToShow: 1,
+//    slidesToScroll: 1,
+//    arrows: false,
+//    // asNavFor: '.hero-slider',
+//    dots: false,
+// });
+
+$('.hero-slider').slick({
+   dots: true,
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   appendDots: $('#heroSliderDots'),
+   asNavFor: $(".hero-slider-ill"),
+   fade: true
+});
+$('.hero-slider-ill').slick({
+   asNavFor: $(".hero-slider"),
+   arrows: false,
+   dots: false
+});
 
 // ? teacher slider
 // ? ====================================================
